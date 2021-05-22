efe8de4a14f2c3bdbac17c716f8188e26bffa5cc
Юрий Ронин <47382042+yurkaronin@users.noreply.github.com>
2021-05-22 20:38:49 +0300

    Добавил часть файлов в исключение

---
+++ build/js/main.js
@@ -0,0 +1,18 @@
+function printName(name, func) {
+    console.log(name);
+    func();
+}
+
+printName('Bob', () => {
+	console.log('Hello from arrow function');
+});
+;(function () {
+    const name = 'Bob';
+    console.log(name);
+})()
+;(function () {
+    const name = 'John';
+    console.log(name);
+})()
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4uanMiLCJtb2R1bGUtYS5qcyIsIm1vZHVsZS1iLmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQSxDQ1JBO0FBQ0E7QUFDQTtBQUNBO0FBQ0EsQ0NKQTtBQUNBO0FBQ0E7QUFDQTtBQUNBIiwiZmlsZSI6Im1haW4uanMiLCJzb3VyY2VzQ29udGVudCI6WyJmdW5jdGlvbiBwcmludE5hbWUobmFtZSwgZnVuYykge1xyXG4gICAgY29uc29sZS5sb2cobmFtZSk7XHJcbiAgICBmdW5jKCk7XHJcbn1cclxuXHJcbnByaW50TmFtZSgnQm9iJywgKCkgPT4ge1xyXG5cdGNvbnNvbGUubG9nKCdIZWxsbyBmcm9tIGFycm93IGZ1bmN0aW9uJyk7XHJcbn0pO1xyXG4iLCIoZnVuY3Rpb24gKCkge1xyXG4gICAgY29uc3QgbmFtZSA9ICdCb2InO1xyXG4gICAgY29uc29sZS5sb2cobmFtZSk7XHJcbn0pKClcclxuIiwiKGZ1bmN0aW9uICgpIHtcclxuICAgIGNvbnN0IG5hbWUgPSAnSm9obic7XHJcbiAgICBjb25zb2xlLmxvZyhuYW1lKTtcclxufSkoKVxyXG4iXX0=
